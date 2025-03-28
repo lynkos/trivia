0a235457d3381a67648908296d623ce6623df606
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-03-28 13:39:47 -0400

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Kiran Brahmatewari on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
